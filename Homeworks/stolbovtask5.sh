4c4d785375b7a246a9f4466bb83c8067614d75c6
Stolbov Andrey <stolbov.andrey2006@gmail.com>
2024-12-05 01:43:38 +0500

    Update stolbovtask5.sh

--- Homeworks/stolbovtask5.sh
+++ Homeworks/stolbovtask5.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Функция для отображения справки
 show_help() {
     echo "Использование: $0 [опции]"
     echo "Опции:"
@@ -8,10 +7,8 @@ show_help() {
     echo "  -d, --directory     Указать директорию для поиска (по умолчанию текущая)"
 }
 
-# Инициализация переменной для директории
 directory="."
 
-# Обработка аргументов командной строки
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -h|--help) 
@@ -31,18 +28,15 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-# Получение списка открытых файлов с помощью lsof
 open_files=$(lsof +D "$directory" | awk '{print $9}' | sort -u)
 
-# Поиск подкаталогов первого уровня в указанной директории
 find "$directory" -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
-    # Проверка, есть ли открытые файлы в подкаталоге
+
     if ! echo "$open_files" | grep -q "^$dir"; then
         echo "$dir"
     fi
 done
 
-# Установка кода возврата, если ничего не найдено
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
