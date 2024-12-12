8c9c66999f581e62d61d7a74a56cae8164abd2fc
yudeta <9122519939@mail.ru>
2024-12-12 22:39:05 +0500

    task5

---
+++ task5/shardCollect.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function show_help() {
+    echo "Использование: $0 [-h] [-d directory] [--help] [--directory directory]"
+    echo "-d | --directory - указывает директорию."
+    echo "-h | --help - вывод справки об использовании."
+    echo Примечание: если обрабатываемая директория не указана, то программа будет использовать текущую.
+    exit 0
+}
+
+DIRECTORY="."
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        (-h|--help) show_help ;;
+        (-d|--directory) DIRECTORY="$2"; shift ;;
+        (*) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [ -z "${DIRECTORY}" ]; then
+    DIRECTORY=$(pwd)
+fi
+
+OPEN_DIRS=$(lsof +D "$DIRECTORY" 2>/dev/null | sort | uniq)
+
+for dir in "$DIRECTORY"/*; do
+    if [ -d "$dir" ]; then
+        if ! echo "$OPEN_DIRS" | grep "^$dir$"; then
+            echo "$dir"
+            RESULT=1
+        fi
+    fi
+done
+
+exit $RESULT    
